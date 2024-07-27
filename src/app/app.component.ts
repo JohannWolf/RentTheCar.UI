e753adc39142e6609698ecc618a39f397593c4c4
Juan Nava <jnava9268@gmail.com>
2024-07-26 21:37:36 -0600

    added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
@@ -10,11 +12,21 @@ import { LoginComponent } from './login/login.component';
     RouterOutlet,
     HttpClientModule,
     LoginComponent,
-  ],
+    NavbarComponent,
+    CommonModule
+],
   template: `
+    <ng-container *ngIf="showNavbar()">
+      <app-navbar></app-navbar>
+    </ng-container>
     <router-outlet></router-outlet>
   `,
 })
 export class AppComponent {
   title = 'RentTheCar';
+  private router = inject(Router);
+
+  showNavbar(): boolean {
+    return this.router.url !== '/login';
+  }
 }
